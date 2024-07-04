2844486e858ebad3e5d5830eb3ed2516bf40bd18
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-04 11:15:27 +0300

    Initial commit

--- lib/model/course.dart
+++ lib/model/course.dart
@@ -4,9 +4,5 @@ abstract class Course {
   final int semester;
   final int year;
 
-  Course(String id, String name, int semester, int year)
-      : this.id = id,
-        this.name = name,
-        this.semester = semester,
-        this.year = year;
+  Course(this.id, this.name, this.semester, this.year);
 }
