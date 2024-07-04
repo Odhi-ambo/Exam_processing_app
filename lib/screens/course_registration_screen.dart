a622c1f8e477a346b3e11756f2921b9ff2d52c55
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-04 11:12:58 +0300

    Initial commit

---
+++ lib/screens/course_registration_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CourseRegistrationScreen extends StatelessWidget {
+  const CourseRegistrationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
