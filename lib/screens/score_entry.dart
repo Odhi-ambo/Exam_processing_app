d3278f3a7c3b63bc385b4559c1277d88e3cdc743
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-08 16:46:32 +0300

    Initial commit

---
+++ lib/screens/score_entry.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScoreEntry extends StatefulWidget {
+  const ScoreEntry({super.key});
+
+  @override
+  State<ScoreEntry> createState() => _ScoreEntryState();
+}
+
+class _ScoreEntryState extends State<ScoreEntry> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
