fde1db7bd5a9483c083bf1eb014d132fa36d3371
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-08 16:51:48 +0300

    Initial commit

--- lib/screens/score_entry.dart
+++ lib/screens/score_entry.dart
@@ -1,18 +1,83 @@
+import 'package:exam_processing_app/screens/course_registration_screen.dart';
 import 'package:flutter/material.dart';
 
-class ScoreEntry extends StatefulWidget {
-  const ScoreEntry({super.key});
-
+class ScoreEntryScreen extends StatefulWidget {
   @override
-  State<ScoreEntry> createState() => _ScoreEntryState();
+  _ScoreEntryScreenState createState() => _ScoreEntryScreenState();
 }
 
-class _ScoreEntryState extends State<ScoreEntry> {
+class _ScoreEntryScreenState extends State<ScoreEntryScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late String selectedCourse;
+  late int assignment1, assignment2, cat1, cat2, exam;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Score Entry'),
+      appBar: AppBar(title: Text("Enter Scores")),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            DropdownButtonFormField(
+              items: courses.map((course) {
+                return DropdownMenuItem(
+                  value: course.id,
+                  child: Text(course.name),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCourse = value!;
+                });
+              },
+              decoration: InputDecoration(labelText: "Select Course"),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Assignment 1"),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                assignment1 = int.parse(value);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Assignment 2"),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                assignment2 = int.parse(value);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "CAT 1"),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                cat1 = int.parse(value);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "CAT 2"),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                cat2 = int.parse(value);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Exam"),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                exam = int.parse(value);
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // Save scores to database
+                }
+              },
+              child: Text("Submit Scores"),
+            ),
+          ],
+        ),
       ),
     );
   }
