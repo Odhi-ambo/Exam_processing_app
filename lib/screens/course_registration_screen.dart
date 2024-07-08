bf378bf102c7687196d0992f04d5e1b222c3a7d4
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-08 16:43:30 +0300

    Initial commit

--- lib/screens/course_registration_screen.dart
+++ lib/screens/course_registration_screen.dart
@@ -1,29 +1,71 @@
 import 'package:flutter/material.dart';
 
-class CourseRegistrationScreen extends StatefulWidget {
-  const CourseRegistrationScreen({super.key});
+class Course {
+  final String id;
+  final String name;
 
+  Course({required this.id, required this.name});
+}
+
+List<Course> courses = [
+  Course(id: '1', name: 'Mathematics'),
+  Course(id: '2', name: 'Physics'),
+  Course(id: '3', name: 'Chemistry'),
+  Course(id: '4', name: 'Biology'),
+  Course(id: '5', name: 'Computer Science'),
+  // Add more courses as needed
+];
+
+class CourseRegistrationScreen extends StatefulWidget {
   @override
-  State<CourseRegistrationScreen> createState() =>
+  _CourseRegistrationScreenState createState() =>
       _CourseRegistrationScreenState();
 }
 
 class _CourseRegistrationScreenState extends State<CourseRegistrationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> selectedCourses = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Course Registration'),
-      ),
+      appBar: AppBar(title: Text("Course Registration")),
       body: Form(
-          child: Column(
-        children: [
-          Expanded(
+        key: _formKey,
+        child: Column(
+          children: [
+            Expanded(
               child: ListView(
-            children: [],
-          ))
-        ],
-      )),
+                children: List.generate(courses.length, (index) {
+                  return CheckboxListTile(
+                    title: Text(courses[index].name),
+                    value: selectedCourses.contains(courses[index].id),
+                    onChanged: (bool? value) {
+                      setState(() {
+                        if (value == true) {
+                          selectedCourses.add(courses[index].id);
+                        } else {
+                          selectedCourses.remove(courses[index].id);
+                        }
+                      });
+                    },
+                  );
+                }),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (selectedCourses.length >= 5) {
+                  // Save to database
+                } else {
+                  // Show error
+                }
+              },
+              child: Text("Register Courses"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
