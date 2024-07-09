00157c367b4c7e6d3e3992a57121dd1fe6955179
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-09 11:29:55 +0300

    Initial commit

---
+++ lib/model/score.dart
@@ -0,0 +1,19 @@
+class Score {
+  final String studentId;
+  final String courseId;
+  final int assignment1;
+  final int assignment2;
+  final int cat1;
+  final int cat2;
+  final int exam;
+
+  Score({
+    required this.studentId,
+    required this.courseId,
+    required this.assignment1,
+    required this.assignment2,
+    required this.cat1,
+    required this.cat2,
+    required this.exam,
+  });
+}
