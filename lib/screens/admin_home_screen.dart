c91d96d1ddf21ed0e550093f508870facfdda725
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-11 11:01:51 +0300

    Initial commit

--- lib/screens/admin_home_screen.dart
+++ lib/screens/admin_home_screen.dart
@@ -5,6 +5,8 @@ class AdminHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Admin Home")),
+    );
   }
 }
